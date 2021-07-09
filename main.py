f862cab6499650f09fe6075bc9ce1879f4970728
Adedeji438 <85716328+Adedeji438@users.noreply.github.com>
2021-07-09 06:54:47 -0500

    Homework 4

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 # Adedeji Akingbola
-# 1793979
-def get_age():
-    age = int(input())
-    if age < 18 or age > 75:  
-        raise ValueError("Invalid age.")
-    return age
-
-# TODO: Complete fat_burning_heart_rate() function
-def fat_burning_heart_rate(age):
-    heart_rate = (220 - age) * 0.7
-    return heart_rate
-
-if __name__ == "__main__":
-    # TODO: Modify to call get_age() and fat_burning_heart_rate()
-    #       and handle the exception
+# 173979
+# Split input into 2 parts: name and age
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    # FIXME: The following line will throw ValueError exception.
+    #        Insert try/except blocks to catch the exception.
     try:
-        age = get_age()
-        rate = fat_burning_heart_rate(age)
-        print("Fat burning heart rate for a", age, "year-old:", rate, "bpm")
-    except ValueError as e:
-        print(e)
-        print('Could not calculate heart rate info.')
-        print()
+        age = int(parts[1]) + 1
+        print('{} {}'.format(name, age))
+    except ValueError:
+        print('{} 0'.format(name))
+    # Get next line
+    parts = input().split()
+    name = parts[0]
